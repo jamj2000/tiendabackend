74923d84cb3723d0ec06d2e06cb43aa4afe68b02
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2020-01-17 23:14:55 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,55 +1,15 @@
-const { Cliente, Articulo } = require("./models.js");
 const express = require("express");
+const controller = require("./controllers.js");
 
 const router = express.Router();
 
 // --------------- API REST CRUD
 
-// Read All
-router.get("/clientes", (req, res) => {
-  // A partir de aquí es el equivalente al Controller
-  Cliente.find({}, (err, data) => {
-    if (err) res.json({ error: err });
-    else res.json(data);
-  });
-});
 
-// Read
-router.get("/clientes/:categoria/:id", (req, res) => {
-  Cliente.findOne({ _id: req.params.id }, (err, data) => {
-    // La _ de _id se indica porque es un campo de MongoDB automático
-    if (err) res.json({ error: err });
-    else res.json(data);
-  });
-});
-
-// Delete
-router.delete("/clientes/:id", (req, res) => {
-  Cliente.findOneAndRemove({ _id: req.params.id }, (err, data) => {
-    if (err) res.json({ error: err });
-    else res.json(data);
-  });
-});
-
-// Update
-router.put("/clientes/:id", (req, res) => {
-  Cliente.findOneAndUpdate(
-    { _id: req.params.id },
-    { $set: { nombre: req.body.nombre, apellidos: req.body.apellidos } }, // Llama al campo nombre del formulario
-    (err, data) => {
-      if (err) res.json({ error: err });
-      else res.json(data);
-    }
-  );
-});
-
-// Create
-router.post("/clientes", (req, res) => {
-    const cliente = new Cliente( {nombre: req.body.nombre, apellidos: req.body.apellidos} );
-    cliente.save((err, data) => {
-        if (err) res.json({ error: err });
-        else res.json(data);
-      });
-});
+router.get("/clientes", controller.listClientes);          // Read All
+router.get("/clientes/:id", controller.readCliente);       // Read
+router.delete("/clientes/:id", controller.deleteCliente);  // Delete
+router.put("/clientes/:id", controller.updateCliente);     // Update
+router.post("/clientes", controller.createCliente);        // Create
 
 module.exports = router;
